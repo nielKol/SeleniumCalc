afffa1930fe73db47d7655311394bcf06de02534
Danil <niel.kol94@gmail.com>
2017-11-03 13:36:36 +0200

    SeleniumCalc

---
+++ SeleniumCalcTests-master/SeleniumCalcTests-master/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SeleniumCalcF")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SeleniumCalcF")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a4e50d51-6575-45a4-8a50-bf98b520b146")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SeleniumCalcTests-master/SeleniumCalcTests-master/UnitTest1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.IE;
+using OpenQA.Selenium.Opera;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Remote;
+using System.IO;
+using OpenQA.Selenium.Safari;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Selenium
+{
+    [TestClass]
+    public class TestCalc
+    {
+        static IWebDriver driver;
+
+        [ClassInitialize]
+        public static void ClassSetUp(TestContext context)
+        {
+            driver = new ChromeDriver();
+
+           /* //OperaOptions srv = new OperaOptions();
+            //srv.BinaryLocation = @"C:\Program Files\Opera\launcher.exe";
+            //driver = new OperaDriver(srv);
+
+            //FirefoxOptions ffOptions = new FirefoxOptions();
+            //ffOptions.BrowserExecutableLocation = @"C:\Program Files\Mozilla Firefox\firefox.exe";            
+            //driver = new FirefoxDriver(ffOptions);
+
+            //driver = new EdgeDriver();
+
+            //driver = new InternetExplorerDriver();
+
+            //driver = new SafariDriver();
+
+            //driver.Navigate().GoToUrl("file:///C:/my%20stuff/ORT%20courses/Homeworks/jsCalcBtn/CalcBtn.html");*/
+            driver.Navigate().GoToUrl("file:///C:/my%20stuff/ORT%20courses/Homeworks/jsCalcFields/CalcFields.html");
+        }
+
+        [ClassCleanup]
+        public static void ClassTeardown()
+        {
+            driver.Quit();
+        }
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver.Navigate().Refresh();
+        }
+
+
+        [DataTestMethod]
+        [DataRow("aTxt", true)]
+        [DataRow("bTxt", true)]
+        [DataRow("oTxt", true)]
+        [DataRow("resTxt", true)]
+        [DataRow("btnCalc", true)]
+
+
+        [TestMethod]
+        public void TestIsPresent(string id, bool exp)
+        {
+            bool actual = driver.FindElement(By.Id(id)).Displayed;
+            Assert.AreEqual(exp, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("aTxt", "0", "0")]
+        [DataRow("bTxt", "1", "1")]
+        [DataRow("oTxt", "+", "+")]
+
+
+        [TestMethod]
+        public void TestSimpleTest(string id, string key, string exp)
+        {
+            driver.FindElement(By.Id(id)).SendKeys(key);
+            string actual = driver.FindElement(By.Id(id)).GetAttribute("value");
+            Assert.AreEqual(exp, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("aTxt", "12345", "12345")]
+        [DataRow("bTxt", "12345", "12345")]
+        [DataRow("oTxt", "+-*/", "+-*/")]
+
+
+        public void TestComplexTest(string id, string key, string exp)
+        {
+            driver.FindElement(By.Id(id)).SendKeys(key);
+            string actual = driver.FindElement(By.Id(id)).GetAttribute("value");
+            Assert.AreEqual(exp, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow("12", "+", "5", "17")]
+        [DataRow("12", "-", "5", "7")]
+        [DataRow("3", "*", "5", "15")]
+        [DataRow("8", "/", "2", "4")]
+
+
+        [TestMethod]
+        public void TestRealJob(string keyA, string keyO, string keyB, string exp)
+        {
+            driver.FindElement(By.Id("aTxt")).SendKeys(keyA);
+            driver.FindElement(By.Id("oTxt")).SendKeys(keyO);
+            driver.FindElement(By.Id("bTxt")).SendKeys(keyB);
+            driver.FindElement(By.Id("btnCalc")).Click();
+            string actual = driver.FindElement(By.Id("resTxt")).GetAttribute("value");
+            Assert.AreEqual(exp, actual);
+        }
+
+        //[TestMethod]
+        //public void TestJsCalc()
+        //{
+        //    var script = new TestScript();
+        //    script.AppendFile(@"C:\my stuff\ORT courses\Homeworks\jsCalcFields\main.js");
+        //    script.AppendBlock(new JsAssertLibrary());
+
+        //    script.RunTest(@"assert.equal(5, calc(2,3,'+'))");
+        //}
+    }
+}
